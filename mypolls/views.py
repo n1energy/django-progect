85f68e9897c359bb5b8aebcf7bcd357fef5c318c
Ilya <n1oceaninsideme@gmail.com>
2019-07-25 11:21:43 +1000

    small progress

--- mypolls/views.py
+++ mypolls/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Question
 
 def index(request):
-    def index(request):
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
-        context = {'latest_question_list': latest_question_list}
-        return render(request, 'mypolls/index.html', context)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'mypolls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'mypolls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
